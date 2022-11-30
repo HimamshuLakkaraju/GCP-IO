1aec4c5b65a5f879ea6db25273c2971a0b9983f1
HFA22SCM89L <hlakkaraju@hawk.iit.edu>
2022-11-30 04:44:13 -0600

    updated settings

--- src/gcpio/settings.py
+++ src/gcpio/settings.py
@@ -2,9 +2,6 @@ import os
 import sys
 from . import utils
 
-print(sys.argv)
-print("in")
-
 ## Google Drive APIs #
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 CRED_FILE_PATH = os.getenv("CREDENTIALS_PATH")
